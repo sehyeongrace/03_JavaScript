a80a9e42ccbfae334a855fea0de0387bc7622fd1
sehyeongrace <sehyeon.grace@gmail.com>
2025-01-06 14:02:05 +0900

    2024-1-6_조건문 + 실습

--- js/05_조건문.js
+++ js/05_조건문.js
@@ -297,7 +297,7 @@ function check6(){
         // case 3,4,5: // Java만 사용 가능 (JS 안됨)
 
         case 3:  case 4:  case 5: result = "봄"; break;
-        case 6: case 7: case 8: result = "여름"; break;\
+        case 6: case 7: case 8: result = "여름"; break;
         case 9: case 10:  case 11: result = "가을"; break;
         case 12: case 1:   case 2: result = "겨울"; break;
 
